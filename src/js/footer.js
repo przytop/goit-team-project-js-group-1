b7446f9d5e373710e1461fb76c51ed1b8e3f1fec
MagdaLiskiewicz <156846346+MagdaLiskiewicz@users.noreply.github.com>
2024-07-30 17:41:04 +0200

    footer

---
+++ src/js/footer.js
@@ -0,0 +1,56 @@
+const teamLink = document.getElementById('our-team-btn');
+const teamBackdrop = document.querySelector('.team');
+const teamCloseBtn = document.querySelector('.team-close-btn');
+const bodyElement = document.querySelector('body');
+
+teamLink.addEventListener('click', onLinkClick);
+
+function onLinkClick(event) {
+  event.preventDefault();
+
+  teamBackdrop.classList.remove('is-hidden');
+  document.body.classList.add('modal-open');
+
+  addAllEventListeners();
+}
+
+function onEscClick(event) {
+  event.preventDefault();
+
+  if (event.code !== 'Escape') {
+    return;
+  }
+
+  closingModalStaff();
+}
+
+function onBackdropClick(event) {
+  if (event.target.closest('.team-window')) {
+    return;
+  }
+
+  closingModalStaff();
+}
+
+function onCloseBtnClick(event) {
+  event.preventDefault();
+
+  closingModalStaff();
+}
+
+function addAllEventListeners() {
+  document.addEventListener('keydown', onEscClick);
+  teamBackdrop.addEventListener('click', onBackdropClick);
+  teamCloseBtn.addEventListener('click', onCloseBtnClick);
+  bodyElement.style.overflow = 'hidden';
+}
+
+function closingModalStaff() {
+  document.removeEventListener('keydown', onEscClick);
+  teamBackdrop.removeEventListener('click', onBackdropClick);
+  teamCloseBtn.removeEventListener('click', onCloseBtnClick);
+
+  teamBackdrop.classList.add('is-hidden');
+  document.body.classList.remove('modal-open');
+  bodyElement.style.overflow = 'auto';
+}
