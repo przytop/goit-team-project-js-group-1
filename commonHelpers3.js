d88bed4a442838dc7d3fe3c83cc76fecac993883
przytop <102540215+przytop@users.noreply.github.com>
2024-08-05 21:11:20 +0000

    Deploying to gh-pages from @ przytop/goit-team-project-js-group-1@72ae5e8f8eecba96a97d0c6b194d4805190f6496 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,11 +1,11 @@
-import"./assets/main-0dae7c59.js";import"./assets/vendor-f9df95ff.js";const s=document.querySelector(".my-library-movie-list"),o=document.querySelector(".my-library-sorry"),y=document.querySelector(".my-library-main-section .my-library-button"),l=document.querySelector(".my-library-main-section"),a=document.querySelector("#genre");function m(e){const t=document.createElement("li");return t.classList.add("my-library-movie-list-item"),t.innerHTML=`
-      <a href="${e.image}">
-        <img src="${e.image}" class="my-library-movie-picture" />
+import"./assets/main-0dae7c59.js";import"./assets/vendor-f9df95ff.js";const n=document.querySelector(".my-library-movie-list"),o=document.querySelector(".my-library-sorry"),y=document.querySelector(".my-library-main-section .my-library-button"),l=document.querySelector(".my-library-main-section"),a=document.querySelector("#genre");function m(e){const t=document.createElement("li");return t.classList.add("my-library-movie-list-item"),t.innerHTML=`
+      <a href="#">
+        <img src="https://image.tmdb.org/t/p/w500${e.poster_path}" class="my-library-movie-picture" />
         <div class="my-library-gradient"></div>
         <div class="my-library-movie">
           <span class="my-library-movie-title">${e.title}</span>
-          <span class="my-library-movie-genres">${e.genre} | ${e.year}</span>
+          <span class="my-library-movie-genres">${e.genre_ids} | ${e.release_date}</span>
         </div>
       </a>
-    `,t}function i(e=""){s.innerHTML="";const t=JSON.parse(localStorage.getItem("myLibrary"))||[],n=e?t.filter(r=>r.genre.toLowerCase().includes(e.toLowerCase())):t;n.length>0?(l.style.display="block",o.style.display="none",n.forEach(r=>{const c=m(r);s.appendChild(c)})):(o.style.display="block",l.style.display="none")}y.addEventListener("click",function(){i()});a.addEventListener("change",function(){const e=a.value;i(e)});i();
+    `,t}function i(e=""){n.innerHTML="";const t=JSON.parse(localStorage.getItem("myLibrary"))||[],s=e?t.filter(r=>r.genre.toLowerCase().includes(e.toLowerCase())):t;s.length>0?(l.style.display="block",o.style.display="none",s.forEach(r=>{const c=m(r);n.appendChild(c)})):(o.style.display="block",l.style.display="none")}y.addEventListener("click",function(){i()});a.addEventListener("change",function(){const e=a.value;i(e)});i();
 //# sourceMappingURL=commonHelpers3.js.map
