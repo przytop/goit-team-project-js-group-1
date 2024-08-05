d92423e66b1d4e5be7e92b103ec1fb9cc6f44baa
przytop <102540215+przytop@users.noreply.github.com>
2024-08-05 20:19:00 +0000

    Deploying to gh-pages from @ przytop/goit-team-project-js-group-1@9d39dc69d1d42ab77168becc58f6650bbb5a9740 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{T}from"./assets/main-512b0621.js";import"./assets/vendor-f9df95ff.js";document.addEventListener("DOMContentLoaded",async function(){const o=document.querySelector(".catalog-movie-list"),i=new T,p={"Science Fiction":"Sci-Fi"},t=document.querySelector(".backdrop"),f=t.querySelector(".modal-window");function b(){f.innerHTML=`
+import{T}from"./assets/main-00e3f912.js";import"./assets/vendor-f9df95ff.js";document.addEventListener("DOMContentLoaded",async function(){const o=document.querySelector(".catalog-movie-list"),i=new T,p={"Science Fiction":"Sci-Fi"},t=document.querySelector(".backdrop"),f=t.querySelector(".modal-window");function b(){f.innerHTML=`
     <button class="modal-btn-close" type="button">
       <svg class="modal-btn-close-icon" width="10.5" height="10.5" fill="#ffffff">
         <use href=""></use>
