9e52ea7306db318489c2dd8b83e41fa68f723f26
przytop <102540215+przytop@users.noreply.github.com>
2024-08-05 20:43:44 +0000

    Deploying to gh-pages from @ przytop/goit-team-project-js-group-1@bfd2e33b63e48e66f86435490a047152d31027cb 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -7,5 +7,5 @@ import"./assets/main-0dae7c59.js";import"./assets/vendor-f9df95ff.js";const s=do
           <span class="my-library-movie-genres">${e.genre} | ${e.year}</span>
         </div>
       </a>
-    `,t}function i(e=""){s.innerHTML="";const t=JSON.parse(localStorage.getItem("myLibraryMovies"))||[],n=e?t.filter(r=>r.genre.toLowerCase().includes(e.toLowerCase())):t;n.length>0?(l.style.display="block",o.style.display="none",n.forEach(r=>{const c=m(r);s.appendChild(c)})):(o.style.display="block",l.style.display="none")}y.addEventListener("click",function(){i()});a.addEventListener("change",function(){const e=a.value;i(e)});i();
+    `,t}function i(e=""){s.innerHTML="";const t=JSON.parse(localStorage.getItem("myLibrary"))||[],n=e?t.filter(r=>r.genre.toLowerCase().includes(e.toLowerCase())):t;n.length>0?(l.style.display="block",o.style.display="none",n.forEach(r=>{const c=m(r);s.appendChild(c)})):(o.style.display="block",l.style.display="none")}y.addEventListener("click",function(){i()});a.addEventListener("change",function(){const e=a.value;i(e)});i();
 //# sourceMappingURL=commonHelpers3.js.map
