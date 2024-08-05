247aab13fab2eb2d5b6494f37959cd31c336bcf7
MagdaLiskiewicz <156846346+MagdaLiskiewicz@users.noreply.github.com>
2024-08-05 23:11:00 +0200

    header
    prawie zrobione
    trzeba dopracować toggla

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -2,9 +2,12 @@ const switcher = document.querySelector('.theme-switcher');
 const root = document.documentElement;
 
 function switchTheme() {
-  const newTheme = root.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+  const newTheme =
+    root.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
   root.setAttribute('data-theme', newTheme);
-  localStorage.setItem('theme', newTheme);  // Save the user's preference
+  localStorage.setItem('theme', newTheme); // Save the user's preference
+
+  switcher.classList.toggle('active');
 }
 
 switcher.addEventListener('click', switchTheme);
