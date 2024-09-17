41ca64da39772fd4778719292121ca2c56de691e
Paweł <102540215+przytop@users.noreply.github.com>
2024-09-17 15:58:39 +0200

    Update vite.config.js
    exclude axios

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig(({ command }) => {
           },
           entryFileNames: 'commonHelpers.js',
         },
+        external: ['axios'],
       },
       outDir: '../dist',
     },
